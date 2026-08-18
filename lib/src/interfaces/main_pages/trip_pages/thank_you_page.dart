fb427d61ba9e6010bff447b972c270c7d2993c01
SHIDUL PP <shidulpp@gmail.com>
2026-08-18 15:06:10 +0530

    changes in thank you page

--- lib/src/interfaces/main_pages/trip_pages/thank_you_page.dart
+++ lib/src/interfaces/main_pages/trip_pages/thank_you_page.dart
@@ -1,9 +1,38 @@
+import 'dart:async';
+
 import 'package:driveforme_user/src/data/constants/colour_constants.dart';
 import 'package:driveforme_user/src/data/constants/style_constants.dart';
+import 'package:driveforme_user/src/data/providers/nav_provider.dart';
+import 'package:driveforme_user/src/data/services/navigation_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ThankYouPage extends StatelessWidget {
+class ThankYouPage extends ConsumerStatefulWidget {
   const ThankYouPage({super.key});
+
+  @override
+  ConsumerState<ThankYouPage> createState() => _ThankYouPageState();
+}
+
+class _ThankYouPageState extends ConsumerState<ThankYouPage> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {
+      if (mounted) {
+        ref.read(selectedIndexProvider.notifier).updateIndex(0);
+        NavigationService().resetToNavbar();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +45,7 @@ class ThankYouPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Spacer(flex: 2),
-              Image.asset(
-                'assets/pngs/thank_you.png',
-                fit: BoxFit.contain,
-              ),
+              Image.asset('assets/pngs/thank_you.png', fit: BoxFit.contain),
               const SizedBox(height: 40),
               Text(
                 'Thank You!',
